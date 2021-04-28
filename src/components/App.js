7f5d486655412ba7b979932d42bc67637ec3ced6
bykovskaya <irinagomel99@gmail.com>
2021-04-29 00:48:51 +0300

    added Message, socket conponents

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import io from 'socket.io-client'   ; 
+import RegLogForm from "./RegLogForm.js";
+import Message from "./Message"
+import socket from "./socket";
 
-const socket = io.connect('http://localhost:8888');
 socket.on('eventClient', function (data) {
     console.log(data);});
 
@@ -9,77 +10,27 @@ class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-            user:{id:undefined, login:'guest'}
+            status: 'guest',
+            user:{},
+            msgs:[]
         }
+        this.getUser = this.getUser.bind(this);
+    }
+
+    getUser(aUser){
+      this.setState({user:aUser, status:'user'});
     }
 
     render(){
-        return <MyForm />
+        return <div>
+          <div className='reglog'><RegLogForm onLogin={this.getUser}/></div>
+          <div className='chat'>{
+            this.state.msgs.map((msg, i)=>{
+                return <Message key={i} data={msg} />
+            })
+          }</div>
+          </div>
     }
 }
 
 export default App; 
-
-class MyForm extends React.Component {
-
-    constructor(props) {
-      super(props);
-      this.handleSubmit = this.handleSubmit.bind(this);
-      this.addGood = this.addGood.bind(this);
-      this.changeGoodCount = this.changeGoodCount.bind(this);
-      this.state = { goods: [] };
-    }
-  
-    handleSubmit() {
-      console.log('handleSubmit');
-    }
-  
-    addGood() {
-      const { goods } = this.state;
-      this.setState({ goods: [ ...goods, { count: 0 } ] });
-    }
-  
-    changeGoodCount(n, count) {
-      const { goods } = this.state;
-      this.setState({ goods: goods.map( (good, i) => i === n ? { count } : good ) });
-    }
-  
-    render() {
-      const { goods } = this.state;
-      const { handleSubmit, addGood, changeGoodCount } = this;
-      console.log('MyForm.render() -> goods =', goods);
-      return (
-        <form onSubmit={handleSubmit}>
-            <div>
-            Имя пользователя: <input type="text" />
-          </div>
-          <div>
-            Телефон: <input type="text" />
-          </div>
-          <div>
-            { goods.map( (good, i) => <GoodInput key={i} good={good} n={i} onChange={changeGoodCount}/> ) }
-          </div>
-          <div>
-            <button type="button" onClick={addGood}>Добавить что-либо</button>
-          </div>
-        </form>
-      );
-    }
-  }
-  
-  class GoodInput extends React.Component {
-    handleChange(e) {
-      const { n, onChange } = this.props;
-      const count = e.currentTarget.value;
-      onChange(n, count);
-    }
-  
-    render() {
-      const { good, n } = this.props;
-      return ( 
-        <div>
-          Количество товара {n} <input type="text" onChange={(e) => this.handleChange(e)} />
-        </div>
-      );
-    }
-  }
