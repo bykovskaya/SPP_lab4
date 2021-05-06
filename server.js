9cdf1ee8c847bd86994521a1c9b390e121ec9465
biv <biv@omp-system.ru>
2021-05-06 12:58:00 +0300

    initialised sessions

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const port = 8888;
 
 const express = require('express'); 
 const app = express();
+const session = require('express-session');
 const server = app.listen(port, ()=>{print(`server started on port ${port}`)});
 const io = require('socket.io')(server);
 const ss = require('socket.io-stream');
@@ -13,6 +14,14 @@ const jwt = require('jsonwebtoken');
 
 /*attaches static files to page*/
 app.use(express.static(__dirname + "/resources"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(session({
+    secret: 'secret key',
+    saveUninitialized: true,
+    cookie: 'token=12345678',
+}))
 /*******************************/
 
 app.get('/', (req, res) => {
@@ -22,20 +31,20 @@ app.get('/', (req, res) => {
 /*******************************************************/
 /*************************DB****************************/
 /*******************************************************/
-const mysql = require("mysql2");
-const DBconnection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "lab4",
-    password: "er54z4q9"
-});
+// const mysql = require("mysql2");
+// const DBconnection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     database: "lab4",
+//     password: "er54z4q9"
+// });
 
-DBconnection.connect((err) => {
-    if (err)
-        return console.error("Error: " + err.message);
-    else
-        console.log("(DB)Successfully connected!");
-});
+// DBconnection.connect((err) => {
+//     if (err)
+//         return console.error("Error: " + err.message);
+//     else
+//         console.log("(DB)Successfully connected!");
+// });
 
 let getMsgs = () => {
     let sql = "select * from Message";
@@ -100,53 +109,46 @@ let addUser = (args) => {
 /*******************************************************/
 
 // Клиент соединился
-io.sockets.on('connection', function (socket) {
-    print('A client is connected!');
-
-    socket.on('messages', ()=>{
-        //socket.emit('messages', msgs);
-    });
-
-    socket.on('registrate', (data)=>{
-        print('registrate', data.login, data.password);
-        let args = [data.login];
-
-        bcrypt.hash(data.password, 10, (err, hashedPassword) => {
-            if (err) {
-                console.log(err);
-                socket.emit('registrate', { message: 'Password encription error'});
-            }
-            else {
-                args.push(hashedPassword);
-                addUser(args)
-                .then ( ()   => { socket.emit('registrate', { message: 'You are registred!' })})
-                .catch((err) => { socket.emit('registrate', { message: 'Registration error', data:err })});
-            }
-        })
-    })
-
-    socket.on('login', (data)=>{
-        print('login', data);
-
-        getUser(data.login)
-        .then(res => {
-            bcrypt.compare(res.password, data.password, (err, equal) => {
-                if (err) {
-                    console.log(err);
-                    socket.emit('login', { message: 'Invalid password.' });
-                }
-                else {
-                    let token = '';
-                    const accessTocken = jwt.sign(token, TOKEN_KEY);
-                    console.log(accessTocken);
-                    socket.emit('login', { message: 'Welcome!', id: res.id, login: res.login });
-                }
-            })
-        })
-        .catch(err => {
-            socket.emit('login', { message: err });
-        });
-
-        
-    })
-});
+// io.on('connection', function (socket) {
+//     print('A client is connected!');
+//     socket.on('messages', ()=>{
+//         //socket.emit('messages', msgs);
+//     });
+//     socket.on('registrate', (data)=>{
+//         print('registrate', data.login, data.password);
+//         let args = [data.login];
+//         bcrypt.hash(data.password, 10, (err, hashedPassword) => {
+//             if (err) {
+//                 console.log(err);
+//                 socket.emit('registrate', { message: 'Password encription error'});
+//             }
+//             else {
+//                 args.push(hashedPassword);
+//                 addUser(args)
+//                 .then ( ()   => { socket.emit('registrate', { message: 'You are registred!' })})
+//                 .catch((err) => { socket.emit('registrate', { message: 'Registration error', data:err })});
+//             }
+//         })
+//     })
+//     socket.on('login', (data)=>{
+//         print('login', data);
+//         getUser(data.login)
+//         .then(res => {
+//             bcrypt.compare(res.password, data.password, (err, equal) => {
+//                 if (err) {
+//                     console.log(err);
+//                     socket.emit('login', { message: 'Invalid password.' });
+//                 }
+//                 else {
+//                     let token = '';
+//                     const accessTocken = jwt.sign(token, TOKEN_KEY);
+//                     console.log(accessTocken);
+//                     socket.emit('login', { message: 'Welcome!', id: res.id, login: res.login });
+//                 }
+//             })
+//         })
+//         .catch(err => {
+//             socket.emit('login', { message: err });
+//         });
+//     })
+// });
